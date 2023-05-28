6b3c3967d24b8a39e80184ee2fa9dbc72d2dfc01
4eDo <61490588+4eDo@users.noreply.github.com>
2023-05-28 20:48:55 +0300

    Upd
    upd

---
+++ feedback-srv/src/test/java/lct/feedbacksrv/FeedbackSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package lct.feedbacksrv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackSrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
